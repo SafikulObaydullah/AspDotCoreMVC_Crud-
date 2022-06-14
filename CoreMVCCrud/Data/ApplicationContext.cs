65dc1f4b2fe826935bacb25743249c0177f782ff
obaydullah <obaydullah599@gmail.com>
2022-06-14 15:47:57 +0600

    COREMVC crud

---
+++ CoreMVCCrud/Data/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using CoreMVCCrud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreMVCCrud.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ CoreMVCCrud/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreMVCCrud.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
